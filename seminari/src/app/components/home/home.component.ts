5aa67af3bf7606cf36bd5ae961d5c53f92a773e2
julia <[julia.roquet@estudiantat.upc.edu]>
2024-10-22 10:41:43 +0200

    seminari7

---
+++ seminari/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Asegúrate de importar RouterModule
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule, RouterLink], // Importar RouterLink
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent { }
